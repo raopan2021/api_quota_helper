2046814a4e351b5b32621171c6578d830d59c5ab
raopan <raopan@xtfintek.com>
2026-03-18 23:02:05 +0800

    fix: 改用HTTPS替代HTTP，版本1.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.apiapp.api_quota_helper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
