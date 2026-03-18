ef2dc2e21a6598c93dde47bd0d344323224e3381
raopan <raopan@xtfintek.com>
2026-03-19 00:13:22 +0800

    feat: 设置改为独立页面，日志全屏可复制，版本1.0.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.apiapp.api_quota_helper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.0.11"
+        versionCode = 13
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
