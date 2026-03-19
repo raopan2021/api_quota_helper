9b8669efe8490be6e22e912bc422e5467e2b5237
raopan <raopan@xtfintek.com>
2026-03-19 17:08:46 +0800

    build: 版本升至 v1.0.38

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.apiapp.api_quota_helper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 34
-        versionName = "1.0.37"
+        versionCode = 35
+        versionName = "1.0.38"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
