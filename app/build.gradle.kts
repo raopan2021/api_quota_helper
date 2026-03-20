44f1cec860cada60acd905efed686da3345d63c7
raopan <raopan@xtfintek.com>
2026-03-20 11:00:43 +0800

    chore: bump to v1.0.51

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.apiapp.api_quota_helper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 45
-        versionName = "1.0.48"
+        versionCode = 46
+        versionName = "1.0.51"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
